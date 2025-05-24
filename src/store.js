f2ab2e0386543408d22e9798ccf5b4050a423e7c
Reuel <reuelc@gmail.com>
2025-05-24 09:46:42 +0800

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,34 +1,10 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './CartSlice';
 
-export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-      const { name, image, cost } = action.payload;
-      const existingItem = state.items.find(item => item.name === name);
-      if (existingItem) {
-        existingItem.quantity++;
-      } else {
-        state.items.push({ name, image, cost, quantity: 1 });
-      }
-    },
-    removeItem: (state, action) => {
-      state.items = state.items.filter(item => item.name !== action.payload);
-    },
-    updateQuantity: (state, action) => {
-      const { name, quantity } = action.payload;
-      const itemToUpdate = state.items.find(item => item.name === name);
-      if (itemToUpdate) {
-        itemToUpdate.quantity = quantity;
-      }
-    },
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
   },
 });
 
-export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
-
-// Export the reducer as default - this is what gets imported in store.js
-export default CartSlice.reducer;
+export default store;
